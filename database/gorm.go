233dc9c2f228814232535e999f0d46bfb319f780
mike <31948147+bluemike0823@users.noreply.github.com>
2024-08-24 01:45:58 +0800

    DB 改用 mysql

--- database/gorm.go
+++ database/gorm.go
@@ -5,29 +5,38 @@ import (
 	"goTestProj/config"
 	"goTestProj/models"
 
+	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
 
-var dsn = fmt.Sprintf(
-	"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
-	config.Env.Get("database.host"),
+// var dsn = fmt.Sprintf(
+// 	"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+// 	config.Env.Get("database.host"),
+// 	config.Env.Get("database.user"),
+// 	config.Env.Get("database.password"),
+// 	config.Env.Get("database.name"),
+// 	config.Env.GetInt("database.port"),
+// )
+
+var dsn_mysql = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True",
 	config.Env.Get("database.user"),
 	config.Env.Get("database.password"),
-	config.Env.Get("database.name"),
+	config.Env.Get("database.host"),
 	config.Env.GetInt("database.port"),
-)
+	config.Env.Get("database.name"))
 
 var postgreCon = postgres.New(
 	postgres.Config{
-		DSN:                  dsn,
+		DSN:                  dsn_mysql,
 		PreferSimpleProtocol: true,
 	},
 )
 
 func connectDB() *gorm.DB {
-	db, err := gorm.Open(postgreCon, &gorm.Config{
+	db, err := gorm.Open(mysql.Open(dsn_mysql), &gorm.Config{
+		//  db, err := gorm.Open(postgreCon, &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true,
 			TablePrefix:   "public.",
@@ -45,10 +54,9 @@ func connectDB() *gorm.DB {
 	db.AutoMigrate(&models.Auth{})
 	// err = db.Create(&models.Auth{Username: "admin", Password: "admin"}).Error
 	db.Create(&models.Auth{Username: "admin", Password: "admin"})
-    // if err != nil {
-    //     // panic("無法創建初始資料")
-    // }
-
+	// if err != nil {
+	//     // panic("無法創建初始資料")
+	// }
 
 	return db.Session(&gorm.Session{
 		PrepareStmt: true,
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module goTestProj
 go 1.21.1
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/astaxie/beego v1.12.3 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
@@ -11,6 +12,7 @@ require (
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -43,11 +45,12 @@ require (
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.7 // indirect
 	gorm.io/driver/postgres v1.5.4 // indirect
-	gorm.io/gorm v1.25.5 // indirect
+	gorm.io/gorm v1.25.11 // indirect
 )
